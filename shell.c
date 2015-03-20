0cb4ff97f6c81bf9d5002b6792edfecb98d5d192
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-03-19 22:41:33 -0400

    modified .gitignore

--- shell.c
+++ shell.c
@@ -13,9 +13,8 @@
 
 /**
  * Program that simulates a simple shell.
- * The shell covers basic commands, including builtin commands 
+ * The shell covers basic commands, including builtin commands
  * (cd and exit only), standard I/O redirection and piping (|). 
- 
  */
 
 #define MAX_DIRNAME 100
@@ -30,25 +29,25 @@ int execute_complex_command(command *cmd);
 
 
 int main(int argc, char** argv) {
-	
+
 	char cwd[MAX_DIRNAME];           /* Current working directory */
 	char command_line[MAX_COMMAND];  /* The command */
-	char *tokens[MAX_TOKEN];         /* Command tokens (program name, 
+	char *tokens[MAX_TOKEN];         /* Command tokens (program name,
 					  * parameters, pipe, etc.) */
 
 	while (1) {
 
-		/* Display prompt */		
+		/* Display prompt */
 		getcwd(cwd, MAX_DIRNAME-1);
 		printf("%s> ", cwd);
-		
+
 		/* Read the command line */
 		fgets(command_line, MAX_COMMAND, stdin);
 		/* Strip the new line character */
 		if (command_line[strlen(command_line) - 1] == '\n') {
 			command_line[strlen(command_line) - 1] = '\0';
 		}
-		
+
 		/* Parse the command into tokens */
 		parse_line(command_line, tokens);
 
@@ -56,11 +55,11 @@ int main(int argc, char** argv) {
 		if (!(*tokens)) {
 			continue;
 		}
-		
+
 		/* Construct chain of commands, if multiple commands */
 		command *cmd = construct_command(tokens);
 		//print_command(cmd, 0);
-    
+
 		int exitcode = 0;
 		if (cmd->scmd) {
 			exitcode = execute_simple_command(cmd->scmd);
@@ -76,7 +75,7 @@ int main(int argc, char** argv) {
 		}
 		release_command(cmd);
 	}
-    
+
 	return 0;
 }
 
@@ -85,64 +84,64 @@ int main(int argc, char** argv) {
  * Changes directory to a path specified in the words argument;
  * For example: words[0] = "cd"
  *              words[1] = "csc209/assignment3/"
- * Your command should handle both relative paths to the current 
+ * Your command should handle both relative paths to the current
  * working directory, and absolute paths relative to root,
  * e.g., relative path:  cd csc209/assignment3/
  *       absolute path:  cd /u/bogdan/csc209/assignment3/
  */
 int execute_cd(char** words) {
-	
-	/** 
-	 * TODO: 
-	 * The first word contains the "cd" string, the second one contains 
+
+	/**
+	 * TODO:
+	 * The first word contains the "cd" string, the second one contains
 	 * the path.
 	 * Check possible errors:
-	 * - The words pointer could be NULL, the first string or the second 
+	 * - The words pointer could be NULL, the first string or the second
 	 *   string could be NULL, or the first string is not a cd command
-	 * - If so, return an EXIT_FAILURE status to indicate something is 
+	 * - If so, return an EXIT_FAILURE status to indicate something is
 	 *   wrong.
 	 */
 
 
 
 	/**
-	 * TODO: 
-	 * The safest way would be to first determine if the path is relative 
+	 * TODO:
+	 * The safest way would be to first determine if the path is relative
 	 * or absolute (see is_relative function provided).
-	 * - If it's not relative, then simply change the directory to the path 
+	 * - If it's not relative, then simply change the directory to the path
 	 * specified in the second word in the array.
-	 * - If it's relative, then make sure to get the current working 
+	 * - If it's relative, then make sure to get the current working
 	 * directory, append the path in the second word to the current working
 	 * directory and change the directory to this path.
 	 * Hints: see chdir and getcwd man pages.
 	 * Return the success/error code obtained when changing the directory.
 	 */
-	 
+
 }
 
 
 /**
- * Executes a program, based on the tokens provided as 
+ * Executes a program, based on the tokens provided as
  * an argument.
- * For example, "ls -l" is represented in the tokens array by 
+ * For example, "ls -l" is represented in the tokens array by
  * 2 strings "ls" and "-l", followed by a NULL token.
- * The command "ls -l | wc -l" will contain 5 tokens, 
- * followed by a NULL token. 
+ * The command "ls -l | wc -l" will contain 5 tokens,
+ * followed by a NULL token.
  */
 int execute_command(char **tokens) {
-	
+
 	/**
 	 * TODO: execute a program, based on the tokens provided.
-	 * The first token is the command name, the rest are the arguments 
-	 * for the command. 
+	 * The first token is the command name, the rest are the arguments
+	 * for the command.
 	 * Hint: see execlp/execvp man pages.
-	 * 
+	 *
 	 * - In case of error, make sure to use "perror" to indicate the name
 	 *   of the command that failed.
-	 *   You do NOT have to print an identical error message to what would 
+	 *   You do NOT have to print an identical error message to what would
 	 *   happen in bash.
-	 *   If you use perror, an output like: 
-	 *      my_silly_command: No such file of directory 
+	 *   If you use perror, an output like:
+	 *      my_silly_command: No such file of directory
 	 *   would suffice.
 	 * Function returns only in case of a failure (EXIT_FAILURE).
 	 */
@@ -162,13 +161,13 @@ int execute_nonbuiltin(simple_command *s) {
 	 *   and permissions);
 	 * - redirect stdin/stdout/stderr to the corresponding file.
 	 *   (hint: see dup2 man pages).
-	 * - close the newly opened file descriptor in the parent as well. 
-	 *   (Avoid leaving the file descriptor open across an exec!) 
+	 * - close the newly opened file descriptor in the parent as well.
+	 *   (Avoid leaving the file descriptor open across an exec!)
 	 * - finally, execute the command using the tokens (see execute_command
 	 *   function above).
 	 * This function returns only if the execution of the program fails.
 	 */
-	
+
 
 }
 
@@ -179,75 +178,75 @@ int execute_nonbuiltin(simple_command *s) {
 int execute_simple_command(simple_command *cmd) {
 
 	/**
-	 * TODO: 
+	 * TODO:
 	 * Check if the command is builtin.
-	 * 1. If it is, then handle BUILTIN_CD (see execute_cd function provided) 
+	 * 1. If it is, then handle BUILTIN_CD (see execute_cd function provided)
 	 *    and BUILTIN_EXIT (simply exit with an appropriate exit status).
-	 * 2. If it isn't, then you must execute the non-builtin command. 
-	 * - Fork a process to execute the nonbuiltin command 
+	 * 2. If it isn't, then you must execute the non-builtin command.
+	 * - Fork a process to execute the nonbuiltin command
 	 *   (see execute_nonbuiltin function above).
 	 * - The parent should wait for the child.
 	 *   (see wait man pages).
 	 */
-	
+
 }
 
 
 /**
- * Executes a complex command.  A complex command is two commands chained 
+ * Executes a complex command.  A complex command is two commands chained
  * together with a pipe operator.
  */
 int execute_complex_command(command *c) {
-	
+
 	/**
 	 * TODO:
 	 * Check if this is a simple command, using the scmd field.
 	 * Remember that this will be called recursively, so when you encounter
 	 * a simple command you should act accordingly.
-	 * Execute nonbuiltin commands only. If it's exit or cd, you should not 
-	 * execute these in a piped context, so simply ignore builtin commands. 
+	 * Execute nonbuiltin commands only. If it's exit or cd, you should not
+	 * execute these in a piped context, so simply ignore builtin commands.
 	 */
-	
 
 
-	/** 
-	 * Optional: if you wish to handle more than just the 
-	 * pipe operator '|' (the '&&', ';' etc. operators), then 
-	 * you can add more options here. 
+
+	/**
+	 * Optional: if you wish to handle more than just the
+	 * pipe operator '|' (the '&&', ';' etc. operators), then
+	 * you can add more options here.
 	 */
 
 	if (!strcmp(c->oper, "|")) {
-		
+
 		/**
-		 * TODO: Create a pipe "pfd" that generates a pair of file 
-		 * descriptors, to be used for communication between the 
-		 * parent and the child. Make sure to check any errors in 
+		 * TODO: Create a pipe "pfd" that generates a pair of file
+		 * descriptors, to be used for communication between the
+		 * parent and the child. Make sure to check any errors in
 		 * creating the pipe.
 		 */
 
-			
+
 		/**
 		 * TODO: Fork a new process.
 		 * In the child:
-		 *  - close one end of the pipe pfd and close the stdout 
+		 *  - close one end of the pipe pfd and close the stdout
 		 * file descriptor.
-		 *  - connect the stdout to the other end of the pipe (the 
+		 *  - connect the stdout to the other end of the pipe (the
 		 * one you didn't close).
-		 *  - execute complex command cmd1 recursively. 
-		 * In the parent: 
+		 *  - execute complex command cmd1 recursively.
+		 * In the parent:
 		 *  - fork a new process to execute cmd2 recursively.
 		 *  - In child 2:
-		 *     - close one end of the pipe pfd (the other one than 
-		 *       the first child), and close the standard input file 
+		 *     - close one end of the pipe pfd (the other one than
+		 *       the first child), and close the standard input file
 		 *       descriptor.
-		 *     - connect the stdin to the other end of the pipe (the 
+		 *     - connect the stdin to the other end of the pipe (the
 		 *       one you didn't close).
-		 *     - execute complex command cmd2 recursively. 
+		 *     - execute complex command cmd2 recursively.
 		 *  - In the parent:
-		 *     - close both ends of the pipe. 
+		 *     - close both ends of the pipe.
 		 *     - wait for both children to finish.
 		 */
-		
+
 	}
 	return 0;
 }
